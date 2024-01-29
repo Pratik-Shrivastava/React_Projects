69325bf6b0e85eb0db7a4e732f37f1ea44fc50ee
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-01-29 23:28:27 +0530

    Create  a new test project through react

---
+++ 99_Testing_Project/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='w-60 border-2 text-white mx-auto text-center'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+    </div>
+      
+    </>
+  )
+}
+
+export default App
